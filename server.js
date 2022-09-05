33c6db54e69dcb95e0fb9f007f76f2255b871944
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-09-05 10:13:09 -0500

    added controllers

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-media-api',
@@ -16,9 +16,9 @@ mongoose.connect(
   }
 );
 
-// Use this to log mongo queries being executed!
+
 mongoose.set('debug', true);
 
-// app.use(require('./routes'));
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
